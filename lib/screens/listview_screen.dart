41d57682f323478d7e5413034591cfa3ac809da9
Olaf <olaf.ruvag@gmail.com>
2022-02-22 19:50:48 -0600

    Pull to Refresh now working

--- lib/screens/listview_screen.dart
+++ lib/screens/listview_screen.dart
@@ -10,53 +10,68 @@ class ListViewBuilderScreen extends StatefulWidget {
 }
 
 class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
-  
+
   final List<int> imagesIds = [1,2,3,4,5,6,7,8,9,10];
   final ScrollController scrollController = ScrollController();
   bool isLoading = false;
 
-
   @override
   void initState() {
     super.initState();
+    
     scrollController.addListener(() {
-      // print('${scrollController.position.pixels},${scrollController.position.maxScrollExtent}');
-      if ((scrollController.position.pixels) >= scrollController.position.maxScrollExtent){
-        // add5();
-        fetchData();
-      }
+        // print(' ${scrollController.position.pixels}, ${scrollController.position.maxScrollExtent} ');
+        if ( (scrollController.position.pixels + 500) >= scrollController.position.maxScrollExtent )  {
+          // add5();
+          fetchData();
+        }
     });
+
   }
 
-  Future fetchData() async{
+  Future fetchData() async {
 
-    if (isLoading) return;
+    if ( isLoading ) return;
 
     isLoading = true;
-    setState(() {   });
+    setState(() {});
 
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed( const Duration( seconds: 3 ));
 
     add5();
 
     isLoading = false;
-    setState(() {    });
+    setState(() {});
 
-    if((scrollController.position.pixels + 100) <= scrollController.position.maxScrollExtent) return;
+    if ( scrollController.position.pixels + 100 <= scrollController.position.maxScrollExtent ) return;
+
     scrollController.animateTo(
       scrollController.position.pixels + 120, 
-      duration: const Duration(milliseconds: 300), 
+      duration: const Duration( milliseconds: 300 ), 
       curve: Curves.fastOutSlowIn
     );
+    
   }
 
-  void add5(){
+
+
+  void add5() {
     final lastId = imagesIds.last;
     imagesIds.addAll(
-        [1,2,3,4,5].map((e)=> lastId + e)
+      [1,2,3,4,5].map((e) => lastId + e )
     );
-    setState(() { });
+    setState(() {});
   }
+
+  Future<void> onRefresh() async {
+    await Future.delayed( const Duration( seconds: 2 ));    
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lastId + 1);
+    add5();
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -70,26 +85,35 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
         removeBottom: true,
         child: Stack(
           children: [
-            ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              controller: scrollController,
-              itemCount: imagesIds.length,
-              itemBuilder: (BuildContext context, int index) {
-                return FadeInImage(
-                  width: double.infinity,
-                  height: 300,
-                  fit: BoxFit.cover,
-                  placeholder: const AssetImage('assets/gifs/jar-loading.gif'), 
-                  image: NetworkImage('https://picsum.photos/500/300?image-${index + 1}'),
-                );
-              },
+
+            RefreshIndicator(
+              color: AppTheme.primary,
+              onRefresh: onRefresh,
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                controller: scrollController,
+                itemCount: imagesIds.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return FadeInImage(
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                    placeholder: const AssetImage('assets/gifs/jar-loading.gif'), 
+                    image: NetworkImage('https://picsum.photos/500/300?image=${ imagesIds[index] }')
+                  );
+                },
+              ),
             ),
-            if (isLoading)
+
+
+            if ( isLoading ) 
               Positioned(
                 bottom: 40,
-                left: (size.width*.5) - 30,
+                left: size.width * 0.5 - 30,
                 child: const _LoadingIcon()
-              ),
+              )
+
+
           ],
         ),
       ),
@@ -106,14 +130,13 @@ class _LoadingIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(10),
-      width: 60,
       height: 60,
-      child: const CircularProgressIndicator(
-        color: AppTheme.primary),
+      width: 60,
       decoration: BoxDecoration(
-        shape: BoxShape.circle,
         color: Colors.white.withOpacity(0.9),
+        shape: BoxShape.circle
       ),
+      child: const CircularProgressIndicator( color: AppTheme.primary )
     );
   }
 }
